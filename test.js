0b1bb25d7c20631c1862b7803c2ba083cbed6074
Daniil Nedostup <46461229+DANIILNEDOSTUP@users.noreply.github.com>
2020-11-09 18:36:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const { add } = require('./src/arithmetics');
+const { substract } = require('./src/arithmetics');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('subtract 5 - 3 to equal 2', () => {
+  expect(substract(5, 3)).toBe(2);
 });
